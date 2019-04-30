4c52cc5cf163e29e8601614ac5818e5479177cd8
wzhxyz <wzh2012@outlook.com>
2019-04-30 19:23:06 +0800

    删除多余文件

--- crawl.py
+++
@@ -1,9 +0,0 @@
-import requests
-from lxml import etree
-
-url = 'https://cn.vjudge.net/contest/249239#rank'
-html = requests.get(url).text
-
-selector = etree.HTML(html)
-links = selector.xpath('//*[@id="contest-rank-table"]//tr')
-print(links)
